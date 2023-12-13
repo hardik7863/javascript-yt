226da0fe2605870fddf33c4938d68ea2afa50fcf
hardik batwal <115218598+hardik7863@users.noreply.github.com>
2023-12-13 13:42:10 +0000

    data type discussed

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,8 @@
 const accountId=14453
+//Variables declared by let are only available inside the block where they're defined. 
+//Variables declared by var are available throughout the function in which they're declared.
+//we prefer to use let becoz if we change the value of variable assigned through var 
+//then the value in the whole code will be change
 let accountEmail="hardikbatwal@google.com"
 var accountPassword ="12345"
 accountCity="jaipur"//it is not prefer to declare variable like this
